df749c70a854c729e6de2fdb1d9b8ab81f36c63d
Servo <louis.servelle@gmail.com>
2025-03-28 20:04:03 +0100

    Add proxy tor

--- apps/crawler/internal/crawler.go
+++ apps/crawler/internal/crawler.go
@@ -3,19 +3,21 @@ package crawler
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 
+	"github.com/gocolly/colly/v2"
 	"golang.org/x/net/proxy"
 )
 
 func TorClient() (string, error) {
 	proxyAddr := os.Getenv("TOR_PROXY")
 	if proxyAddr == "" {
-		proxyAddr = "socks5://tor:9050"
+		proxyAddr = "socks5://172.18.0.2:9050"
 	}
 
-	dialer, err := proxy.SOCKS5("tcp", "tor:9050", nil, proxy.Direct)
+	dialer, err := proxy.SOCKS5("tcp", "172.18.0.2:9050", nil, proxy.Direct)
 	if err != nil {
 		panic(err)
 	}
@@ -26,7 +28,7 @@ func TorClient() (string, error) {
 		},
 	}
 
-	resp, err := client.Get("http://check.torproject.org")
+	resp, err := client.Get("http://6nhmgdpnyoljh5uzr5kwlatx2u3diou4ldeommfxjz3wkhalzgjqxzqd.onion/")
 	if err != nil {
 		panic(err)
 	}
@@ -36,4 +38,33 @@ func TorClient() (string, error) {
 	fmt.Println(string(body))
 
 	return string(body), nil
+}
+
+func scrapper() {
+	url := "http://6nhmgdpnyoljh5uzr5kwlatx2u3diou4ldeommfxjz3wkhalzgjqxzqd.onion/" // Remplace par l'URL cible
+
+	// Crée un nouveau collecteur
+	c := colly.NewCollector(
+		colly.AllowedDomains("http://6nhmgdpnyoljh5uzr5kwlatx2u3diou4ldeommfxjz3wkhalzgjqxzqd.onion/"), // à adapter si nécessaire
+	)
+
+	var links []string
+
+	// Callback sur chaque lien trouvé
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		links = append(links, link)
+		fmt.Println("Lien trouvé :", link)
+	})
+
+	// Callback en cas d'erreur
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Erreur lors du scraping :", err)
+	})
+
+	// Lance la visite
+	err := c.Visit(url)
+	if err != nil {
+		log.Fatal("Échec de la visite :", err)
+	}
 }
