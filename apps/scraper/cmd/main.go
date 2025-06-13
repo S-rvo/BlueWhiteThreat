3e45382693f046d7fd754f483b6f89ffd416035a
leo <leo.teissier@numericable.fr>
2025-06-13 13:23:43 +0200

    add everest scrap

--- apps/scraper/cmd/main.go
+++ apps/scraper/cmd/main.go
@@ -6,11 +6,8 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/S-rvo/BlueWhiteThreat/internal/scraper"
 	"github.com/joho/godotenv"
-
-	"github.com/S-rvo/BlueWhiteThreat/internal/db"
-	"github.com/S-rvo/BlueWhiteThreat/internal/deepdarkCTI"
-	"github.com/S-rvo/BlueWhiteThreat/internal/utils"
 )
 
 var DefaultInterval = 30 // 30 min par défaut
@@ -26,26 +23,32 @@ func main() {
     }
 
     log.Printf("Lancement du scraper (interval: %d min)", DefaultInterval)
-    timer := time.NewTicker(time.Duration(DefaultInterval) * time.Minute)
-    defer timer.Stop()
+    ticker := time.NewTicker(time.Duration(DefaultInterval) * time.Minute)
+    defer ticker.Stop()
 
     for {
-        scrapAndStore_DeepDarkCTI()
-        <-timer.C
+        select {
+        case <-ticker.C:
+            log.Printf("⏰ Exécution programmée du scraper à %s", time.Now().Format("15:04:05"))
+            scraper.ScrapEverest()
+        }
     }
 }
 
-func scrapAndStore_DeepDarkCTI() {
-    dbName := utils.GetEnvOrDefault("MONGO_DB", "BlueWhiteThreat")
-    entries, err := deepdarkCTI.ScrapeAll()
-    if err != nil {
-        log.Fatalf("Erreur lors du scrape : %v", err)
-    }
-    err = db.SaveAllEntries(dbName, "deepdarkCTI", entries)
-    if err != nil {
-        log.Fatalf("MongoDB error: %v", err)
-    } else {
-        log.Printf("Scrap sauvegardé dans MongoDB à %s", time.Now().Format(time.RFC3339))
-    }
-}
+// func scrapAndStore_DeepDarkCTI() {
+//     dbName := utils.GetEnvOrDefault("MONGO_DB", "BlueWhiteThreat")
+//     entries, err := deepdarkCTI.ScrapeAll()
+//     if err != nil {
+//         log.Fatalf("Erreur lors du scrape : %v", err)
+//     }
+//     err = db.SaveAllEntries(dbName, "deepdarkCTI", entries)
+//     if err != nil {
+//         log.Fatalf("MongoDB error: %v", err)
+//     } else {
+//         log.Printf("Scrap sauvegardé dans MongoDB à %s", time.Now().Format(time.RFC3339))
+//     }
+// }
 
+func scrapAll() {
+
+}
--- apps/scraper/go.mod
+++ apps/scraper/go.mod
@@ -1,8 +1,30 @@
 module github.com/S-rvo/BlueWhiteThreat
 
-go 1.22.2
+go 1.23.0
 
-require go.mongodb.org/mongo-driver v1.17.3
+toolchain go1.24.4
+
+require (
+	github.com/gocolly/colly v1.2.0
+	go.mongodb.org/mongo-driver v1.17.3
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.3 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/antchfx/htmlquery v1.3.4 // indirect
+	github.com/antchfx/xmlquery v1.4.4 // indirect
+	github.com/antchfx/xpath v1.3.3 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+)
 
 require (
 	github.com/golang/snappy v0.0.4 // indirect
@@ -13,7 +35,7 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/crypto v0.39.0 // indirect
+	golang.org/x/sync v0.15.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
 )
